7e9eb31ba634dc153f4089c797b733a9100e6101
jrm7162@rit.edu <jrm7162@rit.edu>
2022-02-22 10:14:49 -0500

    One solution file

--- client/windows/httpworker/main.cpp
+++ client/windows/httpworker/main.cpp
@@ -5,8 +5,8 @@
 
 
 // Main is used for debug (prints output to stdout)
-int main(){
+//int main(){
 // WinMain is used for release (no gui)
-//int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
 	mainloop();
 }
